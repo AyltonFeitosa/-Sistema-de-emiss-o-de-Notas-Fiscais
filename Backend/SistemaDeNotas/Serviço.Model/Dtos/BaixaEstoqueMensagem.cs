024cf2a86e12aee1ddfe6c43b3118f184dbff0c8
Aylton Feitosa <aylton.of@hotmail.com>
2026-09-09 18:54:00 -0300

    update solution and library

---
+++ Backend/SistemaDeNotas/Serviço.Model/Dtos/BaixaEstoqueMensagem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serviço.Model.Dtos
+{
+    public class BaixaEstoqueMensagem
+    {
+        public int NotaFiscalId { get; set; }
+        public List<ItemBaixaDto> Itens { get; set; } = new();
+    }
+}
---
+++ Backend/SistemaDeNotas/Serviço.Model/Dtos/ItemBaixaDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serviço.Model.Dtos
+{
+    public class ItemBaixaDto
+    {
+        public int ProdutoId { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
